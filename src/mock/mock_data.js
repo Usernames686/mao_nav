c277a036d26b206a31e005bd583506b88e474247
Usernames686 <aaa12345678abcw@qq.com>
2026-04-21 14:19:29 +0800

    chore: 更新导航数据 - 2026/4/21 14:19:29

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -517,7 +517,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
+  "title": "haha导航🐱",
   "search": "bing",
-  "_fileSha": "83035c696da29fa32a75d9b2cc8a81041f5bf822"
+  "_fileSha": "729770edadbdcbf97e7bc5517795f4bf59d98a64"
 }
